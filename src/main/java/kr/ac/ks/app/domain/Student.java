4bf0474133cb0fd1e5e8d00ac1a022c195f6ac8c
Sangkon Han <sigmadream@gmail.com>
2020-05-24 02:36:39 +0900

    Added Student 객체 수정
    - Entity 객체를 JavaBean 으로 변경
    - JavaBean 테스트 코드 추가
    - 불필요한 파일 삭제

--- src/main/java/kr/ac/ks/app/domain/Student.java
+++ src/main/java/kr/ac/ks/app/domain/Student.java
@@ -1,61 +1,21 @@
 package kr.ac.ks.app.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
+import lombok.*;
 
-@Entity
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode(of = "id")
+@Builder
 public class Student {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String name;
     private String email;
     private String phoneNo;
-
-    public Student() {
-    }
-
-    public Student(String name, String email, String phoneNo) {
-        this.name = name;
-        this.email = email;
-        this.phoneNo = phoneNo;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNo() {
-        return phoneNo;
-    }
-
-    public void setPhoneNo(String phoneNo) {
-        this.phoneNo = phoneNo;
-    }
+    @Enumerated(EnumType.STRING)
+    private StudentStatus status;
 }
---
+++ src/test/java/kr/ac/ks/app/domain/StudentTest.java
@@ -0,0 +1,75 @@
+package kr.ac.ks.app.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StudentTest {
+
+    @Test
+    public void testStudentObject() {
+        Student student = new Student();
+        assertThat(student).isNotNull();
+    }
+
+    @Test
+    public void testStudentBean() {
+        // Given
+        String name = "홍길동1";
+        String email = "home@test.com";
+        String phoneNo = "01012345678";
+
+        // When
+        Student student = new Student();
+        student.setName(name);
+        student.setEmail(email);
+        student.setPhoneNo(phoneNo);
+
+        // Then
+        assertThat(student.getName()).isEqualTo(name);
+        assertThat(student.getEmail()).isEqualTo(email);
+        assertThat(student.getPhoneNo()).isEqualTo(phoneNo);
+    }
+
+    @Test
+    public void testStudentBuilder() {
+        // Given
+        String name = "홍길동1";
+        String email = "home@test.com";
+        String phoneNo = "01012345678";
+
+        // When
+        Student student = Student.builder()
+                .name(name)
+                .email(email)
+                .phoneNo(phoneNo)
+                .build();
+
+        // Then
+        assertThat(student.getName()).isEqualTo(name);
+        assertThat(student.getEmail()).isEqualTo(email);
+        assertThat(student.getPhoneNo()).isEqualTo(phoneNo);
+    }
+
+    @Test
+    public void testStudentStatus() {
+        // Given
+        String name = "홍길동1";
+        String email = "home@test.com";
+        String phoneNo = "01012345678";
+        StudentStatus status = StudentStatus.ATTENDING;
+
+        // When
+        Student student = new Student();
+        student.setName(name);
+        student.setEmail(email);
+        student.setPhoneNo(phoneNo);
+        student.setStatus(status);
+
+        // Then
+        assertThat(student.getName()).isEqualTo(name);
+        assertThat(student.getEmail()).isEqualTo(email);
+        assertThat(student.getPhoneNo()).isEqualTo(phoneNo);
+        assertThat(student.getStatus()).isEqualTo(status);
+    }
+}
