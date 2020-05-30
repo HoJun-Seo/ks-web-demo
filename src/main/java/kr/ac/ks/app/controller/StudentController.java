5288c9cfb6529c1e25c522e63c56b2ed2c45eb2c
SeoHoJun <ghwns6659@naver.com>
2020-05-30 23:38:12 +0900

    RESTful api practice

--- src/main/java/kr/ac/ks/app/controller/StudentController.java
+++ src/main/java/kr/ac/ks/app/controller/StudentController.java
@@ -4,13 +4,13 @@ import kr.ac.ks.app.domain.Student;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
     @PostMapping("/api/students/")
-    public ResponseEntity createStudent(){ //ResponseEntity : 반환값을 무조건 json 으로 변환
-        Student student = new Student();
+    public ResponseEntity createStudent(@RequestBody Student student){ //ResponseEntity : 반환값을 무조건 json 으로 변환
         return ResponseEntity.status(HttpStatus.CREATED).body(student);//HTTP 상태코드는 201 로 내보내라
     }
 }
--- src/test/java/kr/ac/ks/app/controller/StudentControllerTests.java
+++ src/test/java/kr/ac/ks/app/controller/StudentControllerTests.java
@@ -1,6 +1,9 @@
 package kr.ac.ks.app.controller;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.ac.ks.app.domain.Student;
+import kr.ac.ks.app.domain.StudentStatus;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,6 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -20,14 +24,51 @@ public class StudentControllerTests {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper; //객체 JSON 변환을 위한 선언
     
     @Test //응답이 JSON 으로 돌아오는지 및 HTTP 응답 코드 201이 나오는지 확인
     public void createStudentHTTPCode() throws Exception {
+
+        Student student = Student.builder()
+                .id(100L)
+                .name("홍길동")
+                .email("hong@test.com")
+                .phoneNo("01012345678")
+                .status(StudentStatus.ATTENDING)
+                .build();
+
         mockMvc.perform(post("/api/students/")
                 .contentType(MediaType.APPLICATION_JSON) //JSON 응답 확인
                 .accept(MediaType.APPLICATION_JSON) //주는 것도 JSON 으로 전달
+                .content(objectMapper.writeValueAsString(student))
         )
                 .andDo(print()) //상태코드와 JSON 응답이 정확한지 확인하기 위한 출력문
                 .andExpect(status().isCreated()); //상태 코드 확인
+    }
+
+    @Test
+    public void createStudentJSONFormat()throws Exception{
+        //학생 정보를 만들어서
+        //결과가 JSON 인지 확인한다.
+        Student student = Student.builder()
+                .id(100L)
+                .name("홍길동")
+                .email("hong@test.com")
+                .phoneNo("01012345678")
+                .status(StudentStatus.ATTENDING)
+                .build();
+
+        //전달해야 한다.(POST)
+        mockMvc.perform(post("/api/students/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(student))) //Object -> JSON 변환
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(jsonPath("name").exists())
+                .andExpect(jsonPath("email").exists())
+                .andExpect(jsonPath("phoneNo").exists())
+                .andExpect(jsonPath("status").exists());
     }
 }
